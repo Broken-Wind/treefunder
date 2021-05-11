443816e3ea4a2f5c6827e37363cd4c636b1ca718
Greg Intermaggio <g.intermaggio@gmail.com>
2021-05-10 17:29:58 -0700

    Listing CRUD

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,2 +1,8 @@
 class Listing < ApplicationRecord
+	validates :title, presence: true, length: { minimum: 2 }
+	validates :tagline, presence: true, length: { minimum: 5 }
+	validates :description, presence: true, length: { minimum: 5 }
+	validates :image, presence: true
+	validates :investment_goal, presence: true
+	validates :valuation, presence: true
 end
